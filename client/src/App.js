a131d9bb60bed1a3d6b93e555a435b0f806d41ff
dangchien <82793130+vdchien98@users.noreply.github.com>
2022-08-05 23:35:14 +0700

    3 commit them sass , bootstrap , axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.scss';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [listOfPosts, setListOfPosts] = useState([]);
+    useEffect(() => {
+        axios.get('http://localhost:3001/posts').then((response) => {
+            // console.log(response.data);
+            setListOfPosts(response.data);
+        });
+    }, []);
+    return (
+        <div className="App">
+            {listOfPosts.map((value, key) => {
+                return (
+                    <>
+                        <div className="card-body post" key={key}>
+                            <h5 className="card-title title">{value.title}</h5>
+                            <p className="card-text body">{value.postText}</p>
+                            <h4 className="footer">{value.username}</h4>
+                        </div>
+                    </>
+                );
+            })}
+        </div>
+    );
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 const db = require('./models');
 app.use(express.json());
+app.use(cors());
+
 //Routes
 const postRouter = require('./routes/Post');
 app.use('/posts', postRouter);
