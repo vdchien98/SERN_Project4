39d8485fc7bae787de085ddf0730c0a5209d6996
dangchien <82793130+vdchien98@users.noreply.github.com>
2022-08-05 22:30:59 +0700

    sencond commit

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
-// const mysql = require('mysql');
-// const cors = require('cors');
 
 const db = require('./models');
+app.use(express.json());
+//Routes
+const postRouter = require('./routes/Post');
+app.use('/posts', postRouter);
+
 db.sequelize.sync().then(() => {
     app.listen(3001, () => {
         console.log('Hello your server is running on port 3001');
